8b0ef65b3f936394b484fca92a8b94e9e5a8d13c
jr <jrdugue@gmail.com>
2019-12-05 21:34:54 -0500

    completed mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,45 @@
 // Test away!
+import React from "react";
+// import * as rtl from "react-testing-library";
+import { fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+import {render} from "@testing-library/react";
+import Controls from "./Controls";
+
+// afterEach(rtl.cleanup);
+
+// describe('Controls', () => {
+//   it('should be defined', () => {
+//     expect(Controls).toBeDefined();
+//   });
+//   //   expect(Controls).toMatchSnapshot();
+//   // });
+//  });
+
+test("provide buttons to toggle the closed and locked states", () => {
+  const {getByText} = render(<Controls />);
+  const lockedbtn  = getByText(/lock gate/i);
+  const closebtn = getByText(/close gate/i);
+
+  expect(lockedbtn).toBeDefined();
+  expect(closebtn).toBeDefined();
+});
+
+test("buttons' text changes to reflect the state the door will be in if clicked", () => {
+
+  const locked = false;
+  const closed = false;
+  const toggleClosed = jest.fn();
+
+  const {getByText, findByText} = render(
+    <Controls locked={locked} closed={closed} toggleClosed={toggleClosed} />)
+
+  const closedBtn = getByText(/close gate/i);
+
+  fireEvent.click(closedBtn);
+
+  const openBtn = findByText(/open gate/i);
+
+  expect(openBtn).toBeDefined();
+  expect(toggleClosed).toHaveBeenCalled();
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,26 @@
 // Test away
+import React from "react";
+import * as rtl from "react-testing-library";
+import "jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+import { render } from "@testing-library/react";
+
+afterEach(rtl.cleanup)
+
+// test("Fake Test", () => {
+//   expect(true).toBeTruthy();
+// })
+
+test("Component renders", () => {
+  render(<Dashboard />);
+});
+
+test("shows controls and display", () => {
+    const { getByText } = render(<Dashboard />);
+
+    const display = getByText(/open/i)
+    const controls = getByText(/close gate/i)
+
+    expect(display).toBeDefined();
+    expect(controls).toBeDefined();
+})
